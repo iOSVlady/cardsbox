4e1b3f68bd433e270a054037d06efe6d2fd40e1d
Vladyslav <46273878+UberVladMan@users.noreply.github.com>
2020-07-08 19:43:28 +0300

    navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { trigger, state, style, animate, transition, animation } from '@angular/animations';
-
-
+import { WOW } from 'wowjs/dist/wow.min';
+import { async } from 'rxjs/internal/scheduler/async';
 
 
 
@@ -24,19 +24,42 @@ import { trigger, state, style, animate, transition, animation } from '@angular/
 
         transition('start => end', animate(600))
         
-        ])
+        ]),
+      trigger('navigation', [
+        state('navbar-white', style({
+          color:'black' 
+        })),
+        state('navbar-black', style({
+          color:'white' 
+        }))
+      
+      ]),
+      trigger('backgrn', [
+        state('bwhite', style({
+          background:'white' 
+        })),
+        state('bblack', style({
+          background:'black', 
+        })),
+        transition('bwhite => bblack', animate(300)),
+        transition('bblack => bwhite', animate(300))
+      ])
   ]
 })
 
 export class HomeComponent implements OnInit {
+  navbar = 'navbar-white'
+  background = 'bwhite'
   boxState = 'start';
   boxState_title = 'start';
   boxState_text = 'start';
-  router: any;
+
+
   ngAfterViewInit() {
     setTimeout(_ => this.animate());
     setTimeout(_ => this.animate_title(), 600);
     setTimeout(_ => this.animate_text(), 1200);
+    
 }
   animate(){
     this.boxState = this.boxState === 'end' ? 'start' : 'end'
@@ -47,20 +70,26 @@ export class HomeComponent implements OnInit {
   animate_text(){
     this.boxState_title = this.boxState_title === 'end' ? 'start' : 'end'
   }
+  animate_navbar(){
+    this.navbar = this.navbar == 'navbar-black' ? 'navbar-white' : 'navbar-black'
+  }
+  animate_background(){
+    this.background = this.background == 'bwhite' ? 'bblack' : 'bwhite'
+  }
+
  
   
-  // private wowSubscription: Subscription;
- constructor(){
- 
-    // this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => {
 
-    // // this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+ 
+  constructor(){
+    // this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+    //   // Reload WoW animations when done navigating to page,
+    //   // but you are free to call it whenever/wherever you like
     //   this.wowService.init(); 
     // });
-    }
- 
+  }
   ngOnInit(): void {
-    // this.wowService.init();
+  
     
   }
   @HostListener("window:scroll", ['$event'])
@@ -68,13 +97,20 @@ export class HomeComponent implements OnInit {
     const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
     if (number > 5) {
       let element = document.getElementById('navbar');
-      element.classList.add('navbar-inverse');
       console.log('You are 100px from the top to bottom');
+      if(this.navbar == "navbar-white"){
+          this.animate_background();
+          this.animate_navbar();        }
+      
     } else {
       let element = document.getElementById('navbar');
-      element.classList.remove('navbar-inverse');
+      if(this.navbar == "navbar-black"){
+      this.animate_navbar();
+      this.animate_background();
+      }
       console.log('You are 500px from the top to bottom');
     }
   }
+  
 }
 // 
